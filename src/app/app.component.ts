8ecfbd9460b99df87edada06fdddce70a762be0c
Tigran Grigoryan <tigran.grigoryan1995@gmail.com>
2018-11-29 19:01:20 +0400

    Completed task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +8,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  form: FormGroup;
+  projectStatuses = ['Stable', 'Critical', 'Finished'];
+  forbiddenProjectNameList = ['Test'];
+
+  constructor() {
+    this.form = new FormGroup({
+      projectName: new FormControl(null, [Validators.required, this.forbiddenProjectNames.bind(this)], [this.forbiddenProjectNamesAsync.bind(this)]),
+      email: new FormControl(null, [Validators.required, Validators.email]),
+      projectStatus: new FormControl()
+    });
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      console.log(this.form.value);
+    }
+  }
+
+  forbiddenProjectNames(control: FormControl) : { [key: string]: boolean } {
+    if (this.forbiddenProjectNameList.includes(control.value)) {
+      return { 'projectNameIsForbidden': true };
+    }
+
+    return null;
+  }
+
+  forbiddenProjectNamesAsync(control: FormControl) : Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (this.forbiddenProjectNameList.includes(control.value)) {
+          resolve({ 'projectNameIsForbiddenAsync': true });
+        } else {
+          resolve(null);
+        }
+      }, 1500)
+    });
+
+    return promise;
+  }
 }
